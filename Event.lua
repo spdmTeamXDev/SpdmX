f10d3532c13f705f1d1575b55dd91de27d1f97e0
SpdmTeamX <zusdrodo@gmail.com>
2025-02-15 17:11:12 +0300

    Create Event.lua

---
+++ Event.lua
@@ -0,0 +1,7 @@
+--=============[ SpdmX ]=============--
+game:GetService("ReplicatedStorage").RE.Holiday:FireServer(unpack("UnlockInfoA"))
+game:GetService("ReplicatedStorage").RE.Holiday:FireServer(unpack("UnlockInfoB"))
+game:GetService("ReplicatedStorage").RE.Holiday:FireServer(unpack("UnlockInfoC"))
+game:GetService("ReplicatedStorage").RE.Holiday:FireServer(unpack("UnlockInfoD"))
+game:GetService("ReplicatedStorage").RE.Holiday:FireServer(unpack("UnlockInfoE"))
+--=============[ End ]=============--
